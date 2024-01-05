cadd6ff4380f76b278f3de05b2e9ff6f3cd8c5a3
Fatielbah <elbahraoy.fatiha@gmail.com>
2024-01-05 23:13:31 +0100

    afficher livres et auteurs

--- LireEnLigne/Controllers/HomeController.cs
+++ LireEnLigne/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace LireEnLigne.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
 		{
